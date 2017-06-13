eaa06fab1534b73d435a0e1bcfd2ab08b11158d5
Hoang Vu <hvu.0x7c3@gmail.com>
2017-06-14 02:22:32 +0700

    fix video urls

--- src/videoNames.js
+++ src/videoNames.js
@@ -1,10 +1,10 @@
 export default {
-    'EME_ GÀ MẸT.mp4': '/videos/EME_ GÀ MẸT.mp4',
-    'EME_ MIẾN TRỘN.mp4': '/videos/EME_ MIẾN TRỘN.mp4',
-    'EME_BÁNH CANH GHẸ.mp4': '/videos/EME_BÁNH CANH GHẸ.mp4',
-    'EME_CHIM QUAY MẮC MẬT.mp4': '/videos/EME_CHIM QUAY MẮC MẬT.mp4',
-    'EME_LẨU HẢI SẢN.mp4': '/videos/EME_LẨU HẢI SẢN.mp4',
-    'EME_TÀO PHỚ.mp4': '/videos/EME_TÀO PHỚ.mp4',
-    'EME_XÔI CÁ RÔ ĐỒNG.mp4': '/videos/EME_XÔI CÁ RÔ ĐỒNG.mp4',
-    'TEST': '/videos/test.mp4'
+    'EME_ GÀ MẸT.mp4': 'videos/EME_ GÀ MẸT.mp4',
+    'EME_ MIẾN TRỘN.mp4': 'videos/EME_ MIẾN TRỘN.mp4',
+    'EME_BÁNH CANH GHẸ.mp4': 'videos/EME_BÁNH CANH GHẸ.mp4',
+    'EME_CHIM QUAY MẮC MẬT.mp4': 'videos/EME_CHIM QUAY MẮC MẬT.mp4',
+    'EME_LẨU HẢI SẢN.mp4': 'videos/EME_LẨU HẢI SẢN.mp4',
+    'EME_TÀO PHỚ.mp4': 'videos/EME_TÀO PHỚ.mp4',
+    'EME_XÔI CÁ RÔ ĐỒNG.mp4': 'videos/EME_XÔI CÁ RÔ ĐỒNG.mp4',
+    'TEST': 'videos/test.mp4'
 };
