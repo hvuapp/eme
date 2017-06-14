22331059b2e426eea3d398e52bff03ce550d2d01
Hoang Vu <hvu.0x7c3@gmail.com>
2017-06-14 19:21:08 +0700

    Update video names

--- src/videoNames.js
+++ src/videoNames.js
@@ -1,7 +1,7 @@
 export default ({
-    'EME_ GÀ MẸT.MP4': [{src:'videos/ga-met.mp4',type:'video/mp4'},{src:'videos/ga-met.webm',type:'video/webm'}],
-    'EME_ MIẾN TRỘN.MP4': [{src:'videos/mien-tron.mp4',type:'video/mp4'},{src:'videos/mien-tron.webm',type:'video/webm'}],
-    'EME_BÁNH CANH GHẸ.MP4': [{src:'videos/banh-canh-ghe.mp4',type:'video/mp4'},{src:'videos/banh-canh-ghe.webm',type:'video/webm'}],
+    'GÃ  MÁº¹T': [{src:'videos/ga-met.mp4',type:'video/mp4'},{src:'videos/ga-met.webm',type:'video/webm'}],
+    'MIÁº¿N TRÁ»N': [{src:'videos/mien-tron.mp4',type:'video/mp4'},{src:'videos/mien-tron.webm',type:'video/webm'}],
+    'BÃ¡NH CANH GHÁº¹': [{src:'videos/banh-canh-ghe.mp4',type:'video/mp4'},{src:'videos/banh-canh-ghe.webm',type:'video/webm'}],
     'EME_CHIM QUAY MẮC MẬT.MP4': [{src:'videos/chim-quay-mac-mat.mp4',type:'video/mp4'},{src:'videos/chim-quay-mac-mat.webm',type:'video/webm'}],
     'EME_LẨU HẢI SẢN.MP4': [{src:'videos/lau-hai-san.mp4',type:'video/mp4'},{src:'videos/lau-hai-san.webm',type:'video/webm'}],
     'EME_TÀO PHỚ.MP4': [{src:'videos/tao-pho.mp4',type:'video/mp4'},{src:'videos/tao-pho.webm',type:'video/webm'}],
