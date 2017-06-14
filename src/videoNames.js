de9254c00921107033af4e2171fb4ec0e1107ae8
Hoang Vu <hvu.0x7c3@gmail.com>
2017-06-14 20:18:28 +0700

    update

--- src/videoNames.js
+++ src/videoNames.js
@@ -2,9 +2,9 @@ export default ({
     'GÃ  MÁº¹T': [{src:'videos/ga-met.mp4',type:'video/mp4'},{src:'videos/ga-met.webm',type:'video/webm'}],
     'MIÁº¿N TRÁ»N': [{src:'videos/mien-tron.mp4',type:'video/mp4'},{src:'videos/mien-tron.webm',type:'video/webm'}],
     'BÃ¡NH CANH GHÁº¹': [{src:'videos/banh-canh-ghe.mp4',type:'video/mp4'},{src:'videos/banh-canh-ghe.webm',type:'video/webm'}],
-    'EME_CHIM QUAY MẮC MẬT.MP4': [{src:'videos/chim-quay-mac-mat.mp4',type:'video/mp4'},{src:'videos/chim-quay-mac-mat.webm',type:'video/webm'}],
-    'EME_LẨU HẢI SẢN.MP4': [{src:'videos/lau-hai-san.mp4',type:'video/mp4'},{src:'videos/lau-hai-san.webm',type:'video/webm'}],
-    'EME_TÀO PHỚ.MP4': [{src:'videos/tao-pho.mp4',type:'video/mp4'},{src:'videos/tao-pho.webm',type:'video/webm'}],
-    'EME_XÔI CÁ RÔ ĐỒNG.MP4': [{src:'videos/xoi-ca-ro-dong.mp4',type:'video/mp4'},{src:'videos/xoi-ca-ro-dong.webm',type:'video/webm'}],
+    'CHIM QUAY MÃ³C MÁº­T': [{src:'videos/chim-quay-mac-mat.mp4',type:'video/mp4'},{src:'videos/chim-quay-mac-mat.webm',type:'video/webm'}],
+    'LÁº©U HÁº£I SÁº£N': [{src:'videos/lau-hai-san.mp4',type:'video/mp4'},{src:'videos/lau-hai-san.webm',type:'video/webm'}],
+    'XÃ´I CÃ¡ RÃ´ ÄÁ»NG': [{src:'videos/xoi-ca-ro-dong.mp4',type:'video/mp4'},{src:'videos/xoi-ca-ro-dong.webm',type:'video/webm'}],
+    //'': [{src:'videos/tao-pho.mp4',type:'video/mp4'},{src:'videos/tao-pho.webm',type:'video/webm'}],
     'TEST': [{src:'videos/test.mp4',type:'video/mp4'},{src:'videos/test.webm',type:'video/webm'}]
 });
